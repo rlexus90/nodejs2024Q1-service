7bef481409adae8d4ba0d0bc0a3a7db38d531495
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-03-10 13:50:06 +0100

    init: init module track

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [UserModule, DatabaseModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
